ef9ca66a335577adba18710cf73d70861b60b9f7
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2026-05-08 18:41:06 +0200

    Remove unused HTTP request definitions and enhance documentation for telemetry tag keys

--- src/WebApi/Telemetry/TagKeys.cs
+++ src/WebApi/Telemetry/TagKeys.cs
@@ -1,8 +1,21 @@
 using Platform.Annotations;
 using WebApi.Database.Models;
 
+// ReSharper disable UnusedMember.Local
+// The private constant TodoId appears unused but is actually referenced by the TagKey attribute
+// for telemetry tag registration, through source generation.
+
+// ReSharper disable UnusedType.Global
+// The TelemetryTagKeys class appears unused but is actually referenced through source generation
+// for telemetry tag registration via the TagKey attributes on its members.
+
 namespace WebApi.Telemetry;
 
+/// <summary>
+/// Provides a centralized collection of keys used for telemetry tagging.
+/// This static class is intended to define constant values that represent
+/// the telemetry tags used throughout the application.
+/// </summary>
 public static class TelemetryTagKeys
 {
     [TagKey(typeof(string), typeof(TodoId))]
